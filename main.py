837116885a90fc2b9532e7965f73563e26b39e3d
PNadiadhara <43886212+PNadiadhara@users.noreply.github.com>
2020-11-11 01:21:07 -0500

    health bar added

--- main.py
+++ main.py
@@ -27,23 +27,50 @@ LASER_PLAYER = pygame.image.load(os.path.join("assets", "laser_yellow.png"))
 
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT)) 
 # all players will inherit from this class, both enemy and player
+# self give access to specific instance
 class Ship:
+    COOLDOWN = 30 #half fps for fire rate of 2 shots/sec
     def __init__(self, x, y, health= 100):
         self.x = x
         self.y = y
         self.health = health
         self.ship_img = None
         self.laser_img = None
-        self.laser = []
+        self.lasers = []
         self.cool_down_counter = 0
 
     def draw(self, window):
             window.blit(self.ship_img, (self.x, self.y))
+            for laser in self.lasers:
+                laser.draw(window)
             # pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
+    def move_lasers(self, velocity, object):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(velocity)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(object):
+                object.health -= 10
+                self.lasers.remove(laser)
+
     def get_width(self):
         return self.ship_img.get_width()
+
     def get_height(self):
         return self.ship_img.get_height()
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x - self.ship_img.get_width(),self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
 class Player(Ship): # Player inherits from player
     def __init__(self, x, y, health = 100):
@@ -53,22 +80,89 @@ class Player(Ship): # Player inherits from player
         self.mask = pygame.mask.from_surface(self.ship_img)# mask creates pxl perfect hitbox
         self.max_health = health
 
+    def move_lasers(self, velocity, objects):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(velocity)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            else:
+                for object in objects:
+                    if laser.collision(object):
+                        objects.remove(object)
+                        self.lasers.remove(laser)
 
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
 
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health / self.max_health) , 10))
 
+class Enemy(Ship):
+    NUMBER_MAP = {
+                "one": (ENEMY_SHIP_1, LASER_1),
+                "two": (ENEMY_SHIP_2, LASER_2),
+                "three": (ENEMY_SHIP_3, LASER_3)
+                }
+
+    def __init__(self, x, y,number, health = 100): 
+        super().__init__(x,y,health)
+        self.ship_img, self.laser_img = self.NUMBER_MAP[number]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    def move(self, velocity):
+        self.y += velocity
+
+class Laser:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self .image = image
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def draw(self, window):
+        window.blit(self.image, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self,height):
+        return not self.y <= height and self.y >= 0
+
+    def collision(self, object):
+        return collide(object, self)
         
+# check if two objects are overriding
+
+def collide(object1, object2):
+    offset_x = object2.x - object1.x
+    offset_y = object2.y - object1.y
+    # returns (x,y) intersection
+    return object1.mask.overlap(object2.mask,(offset_x, offset_y)) != None
 
 # main game function
 def main():
     run = True
     FPS = 60
     clock = pygame.time.Clock()
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    lost_font = pygame.font.SysFont("comicsans", 60)
     player_velocity = 5
 
-    player = Player(300, 650)
+    enemies = [] # in python an array(Swift) is a list
+    wave_length = 5
+    enemy_velocity = 1
+
+    laser_velocity = 4
+
+    # players start game location
+    player = Player(350, 630)
+    lost = False
+    lost_count = 0
 
     # Note this function will be nested in main function so as to not have to pass on the contants of FPS etc.
     def redraw_window():
@@ -84,14 +178,42 @@ def main():
         WINDOW.blit(level_label, (10,10))
         WINDOW.blit(lives_label, (WIDTH - lives_label.get_width() - 10, 10))
         
+
+        for enemy in enemies:
+            enemy.draw(WINDOW)
+
         player.draw(WINDOW)
+
+        if lost:
+            lost_label = lost_font.render("Game Over", 1, (255,255,255))
+                                                      # position:(X,Y)
+            WINDOW.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
 
         pygame.display.update()
 
     # uses frames to update game
     while run:
         clock.tick(FPS)
+
         redraw_window()
+
+        if player.health <= 0 or lives <= 0:
+            lost = True
+            lost_count += 1
+
+        # GameOver Duration atm its 180 frames FPS(60) * 3
+        if lost: 
+            if lost_count > FPS * 3 :
+                run = False
+            else:
+                continue
+        
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100), random.choice(["one", "two", "three"]))
+                enemies.append(enemy)
 
         # checks for events at 60hz
         for event in pygame.event.get():
@@ -108,8 +230,26 @@ def main():
             player.y += player_velocity
         if keys[pygame.K_w] and player.y + player_velocity > 0: # up
             player.y -= player_velocity
+        if keys[pygame.K_SPACE]:
+            player.shoot()
 
+        for enemy in enemies[:]: # NOTE [:] CREATES A COPY OF THE LIST(ARRAY IN SWIFT)
+            enemy.move(enemy_velocity)
+            enemy.move_lasers(laser_velocity, player)
+           
+                # gives odds of shooting 25% of the time every second
+            if random.randrange(0, 4*60) == 1:
+                enemy.shoot()
 
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
+            elif enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+
+        player.move_lasers(-laser_velocity, enemies)
+        
         
 main()
 
